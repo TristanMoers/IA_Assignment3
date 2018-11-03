014cf3350de9daedaba2086e344eab1de354310d
TristanMoers <tristan.moers@hotmail.com>
2018-11-03 12:25:31 +0100

    add smart_agent (not working just skeleton)

---
+++ franaubry-tak-public-12a84f0ec28c/smart_agent.py
@@ -0,0 +1,53 @@
+from agent import AlphaBetaAgent
+import minimax
+
+"""
+Agent skeleton. Fill in the gaps.
+"""
+class MyAgent(AlphaBetaAgent):
+
+  """
+  This is the skeleton of an agent to play the Tak game.
+  """
+  def get_action(self, state, last_action, time_left):
+    self.last_action = last_action
+    self.time_left = time_left
+    return minimax.search(state, self)
+
+  """
+  The successors function must return (or yield) a list of
+  pairs (a, s) in which a is the action played to reach the
+  state s.
+  """
+  def successors(self, state):
+        actions = state.get_current_player_actions() 
+        actions_state = list()
+        for action in actions:
+          if state.is_action_valid(action):  
+            Nstate = state.copy()
+            Nstate.apply_action(action)
+            actions_state.append((action, Nstate))
+        for a in actions_state:
+            yield a
+
+  """
+  The cutoff function returns true if the alpha-beta/minimax
+  search has to stop and false otherwise.
+  """
+  def cutoff(self, state, depth):
+    return state.game_over_check() or depth == 1
+
+  """
+  The evaluate function must return an integer value
+  representing the utility function of the board.
+  """
+  def evaluate(self, state):
+    isOver, win = state.is_over()
+    if isOver:
+        if state.get_winner() == self.id:
+            return 1000
+        elif state.get_winner() == 1 - self.id:
+            return -1000
+    else:
+        return state.control_count()[self.id] - state.control_count()[1-self.id]
+
